d302dc1eebe78fc025c22ca4fe488764d809f1fd
todoroki <s18.todoroki+git@gmail.com>
2020-04-25 19:09:17 +0900

    fix warnings of lint

--- section1/char_count.go
+++ section1/char_count.go
@@ -6,7 +6,7 @@ func CharCount(s string) []int  {
 	slice := strings.Split(s, " ")
 	rep := strings.NewReplacer(",", "", ".", "")
 	var counts []int
-	for i, _ := range slice {
+	for i := range slice {
 		text := rep.Replace(slice[i])
 		counts = append(counts, len(text))
 	}
--- section1/map_dictionary.go
+++ section1/map_dictionary.go
@@ -5,7 +5,7 @@ import "strings"
 func MapDictionary(s string) map[string]int  {
 	slice := strings.Split(s, " ")
 	var maps = make(map[string]int)
-	for i, _ := range slice {
+	for i := range slice {
 		text := slice[i]
 		if contains([]int{0, 4, 5, 6, 7, 8, 14, 15, 18}, i) {
 			maps[text[:1]] = i + 1
